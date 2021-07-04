71cc0a312a207541922ed226df2e0b7341f60688
pnofrc <pnofrc@MacBook-Pro.local>
2021-07-04 12:58:19 +0200

    check done cokies

--- app.js
+++ app.js
@@ -25,171 +25,160 @@ var dictioPlaces = {
     '1646': [[52.072945249401826, 4.311648795181019], '<br><p>Dolor sit Amet</p>', 'West Den Haag'],
     'SculptureGallery': [[52.07672464696959, 4.3124565638957835], '<br><p>Dolor sit Amet</p>', '1646']
     }
-  // console.log(coord)
-      // coord.forEach(element => {
-        //   console.log(Object.keys(coord))
-      // });
+
+  let generatedPoet = 'wela  bella gente'
+
+  // to find keys of dictionary
+  // console.log(Object.keys(dictioPlaces))
   
-      var colori = ['yellow','red','black','purple'];
-      let r = Math.floor(Math.random() * (colori.length ))
-      console.log(r)
+  // random color picker
+  var colori = ['yellow','red','black','purple'];
+  let r = Math.floor(Math.random() * (colori.length ))
+  let r2 = Math.floor(Math.random() * (colori.length ))
+  let r3 = Math.floor(Math.random() * (colori.length ))
+
+
+  // MAP 
+  let start = [52.0793046656365, 4.313155972254871]
+  var map = L.map('map', { scrollWheelZoom: true }).setView(start, 13);
   
-  
-  
-      // MAP 
-  
-      let start = [52.0793046656365, 4.313155972254871]
-  
-      //  create map object, tell it to live in 'map' div and give initial latitude, longitude, zoom values 
-      var map = L.map('map', { scrollWheelZoom: true }).setView(start, 13);
-  
-  
-  
-      //  add base map tiles from OpenStreetMap and attribution info to 'map' div
-      // var layer = L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-      //     attribution: '<a href="http://osm.org/copyright">&copy; OpenStreetMap</a>'
-      // }).addTo(map);
-  
-      L.tileLayer("https://api.maptiler.com/maps/basic/256/{z}/{x}/{y}.png?key=WqL6ymt9YHsM4S1RmjmG", {
-      maxZoom: 18,
-      attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
+  // TILES
+  L.tileLayer("https://api.maptiler.com/maps/basic/256/{z}/{x}/{y}.png?key=WqL6ymt9YHsM4S1RmjmG", {
+    maxZoom: 18,
+    attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
   }).addTo(map);
   
+  // QR button  
+  L.easyButton('fa-qrcode fa-2x', function(btn, map){
+    $( "#reader" ).fadeToggle()
+    $( "#reader" ).css("display","flex")
+    $( "#reader" ).css("justify-content","center")
+    $( "#reader" ).css("align-items","center")
+  }, 'qr', 'qr').addTo(map);
+  
+  // BACK HOME
+  L.easyButton("fa-arrow-left fa-2x",function(btn,map){
+    $('#map').slideUp()
+    $( "#reader" ).fadeOut()
+  }, 'menu/mappa', 'menu/mappa').addTo(map);
+  
+  // ICONS
+  var prevIcon = L.icon({
+    iconUrl: 'location.png',
+    iconSize: [50, 50],
+  });
 
-
-  
-      L.easyButton('fa-qrcode fa-2x', function(btn, map){
-      $( "#reader" ).fadeToggle()
-      $( "#reader" ).css("display","flex")
-      $( "#reader" ).css("justify-content","center")
-      $( "#reader" ).css("align-items","center")
-    }, 'qr', 'qr').addTo(map);
-  
-  
-    L.easyButton("fa-arrow-left fa-2x",function(btn,map){
-      $('#map').slideUp()
-      $( "#reader" ).fadeOut()
-  
-    }, 'menu/mappa', 'menu/mappa').addTo(map);
-  
-  
-            // TO CHECK STORED STUFF 
-  
-        let cookies = []
-        let cookiesDone = []
-        let cookiesChanged = []
-        let coo = Cookies.get();
-        let biscottino = Object.keys(coo)
-  
-  
-  
-  
-  
-  
-      var prevIcon = L.icon({
-            iconUrl: 'location.png',
-            iconSize: [50, 50], // size of the icon
-          //   popupAnchor: [0, -15]
-          });
-
-    var doneIcon = L.icon({
+  var doneIcon = L.icon({
     iconUrl: 'location7.png',
-    iconSize: [50, 50], // size of the icon
-    //   popupAnchor: [0, -15]
-    });
-  
-        biscottino.forEach(place => {
-          $(`.${place}`).css('background-color','red')
-          cookies.push(place)
-          cookiesChanged.push(place)
-        });
-  
-  
-        
-        function add(place){
-          $(`.${place}`).css('background-color','red')
-          Cookies.set(place, 0,   { expires: 30 })
-          cookies.push(place)
-          let coo = Cookies.get();
-          let biscottino = Object.keys(coo)
-          console.log(biscottino)
-        }
-  
-        function less(place){
-          $(`.${place}`).css('background-color','rgba(255,214,0)');
-          Cookies.remove(place);
-          cookies.splice( $.inArray(place, cookies), 1 );
-          let coo = Cookies.get();
-          let biscottino = Object.keys(coo)
-          console.log(cookiesChanged)
-          console.log(biscottino)
-          //iconsMap()
-        }
-      
-        
-  
-  
-  
-        function checkCookies(place){
-          if (cookies.includes(place)){
-            less(place)
-            //reload()
-          } else {
-            add(place)
-            //reload()
-          }
-        }
+    iconSize: [50, 50],
+  });
+
+
+  // TO CHECK STORED STUFF 
+  let cookies = []
+  let cookiesDone = []
+  let cookiesChanged = []
+  let coo = Cookies.get();
+  let biscottino = Object.keys(coo)
+
+  for (let vc = 0; vc < biscottino.length; vc++){
+    let value = Object.values(coo)[vc]
+    console.log(value)
+    if ( value == 0){
+      cookies.push(biscottino[vc])
+      console.log('ancora da fare')
+    } else{
+      let key = Object.keys(coo)[vc]
+      cookiesDone.push(key)
+      console.log(key)
+      console.log('andato')
+
+    }
+  }
+
+  // console.log(cookiesDone)
+
+  //TO CHECK SELECTED PLACES
+  cookies.forEach(place => {
+    $(`.${place}`).css('background-color','red')
+    // cookies.push(place)
+    cookiesChanged.push(place)
+  });
+
+  cookiesDone.forEach(cookieDon => {
+    $(`.${cookieDon}`).css('background-color','white')
+    $(`.${cookieDon}`).css('color','black')
+  })
+
+  // TO ADD 
+  function add(place){
+    $(`.${place}`).css('background-color','red')
+    Cookies.set(place, 0,   { expires: 30 })
+    cookies.push(place)
+    let coo = Cookies.get();
+    let biscottino = Object.keys(coo)
+    console.log(biscottino)
+  }
+
+  // TO REMOVE
+  function less(place){
+    $(`.${place}`).css('background-color','rgba(255,214,0)');
+    Cookies.remove(place);
+    cookies.splice( $.inArray(place, cookies), 1 );
+    let coo = Cookies.get();
+    let biscottino = Object.keys(coo)
+    console.log(cookiesChanged)
+    console.log(biscottino)
+    //iconsMap()
+  }
+    
+  // LOGIC PLACES      
+  function checkCookies(place){
+    if (cookies.includes(place)){
+      less(place)
+    } else {
+      add(place)
+    }
+  }
   
    
-      console.log(cookies)
       
-      
-      cookies.forEach(element => {
+  // CREATE MARKER FOR EACH COOKIE (OPEN)   
+  cookies.forEach(element => {
+    var markerOptions = {
+        title: element,
+        clickable: true,
+        icon: prevIcon
+        }
+
+        let lon = dictioPlaces[element][0][0];
+        let lat = dictioPlaces[element][0][1];
+
+        let markerLocation = new L.LatLng(lon,lat);
+        let marker = new L.Marker(markerLocation, markerOptions).bindPopup(`<h1>${dictioPlaces[element][2]}</h1><br>`+dictioPlaces[element][1]+`<br><button class="btn-success btn-marker"  onclick=Cookies.remove("${element}");location.reload()>Remove</button>`).addTo(map);
+
+        map.addLayer(marker)
+      });
+
+  // CREATE MARKER FOR EACH COOKIE (DONE)
+  cookiesDone.forEach(element => {
       var markerOptions = {
           title: element,
           clickable: true,
-          icon: prevIcon
+          icon: doneIcon
           }
           console.log(element)
       
           let lon = dictioPlaces[element][0][0];
           let lat = dictioPlaces[element][0][1];
   
-          // console.log(lon,lat)
-  
           let markerLocation = new L.LatLng(lon,lat);
-           let marker = new L.Marker(markerLocation, markerOptions).bindPopup(`<h1>${dictioPlaces[element][2]}</h1><br>`+dictioPlaces[element][1]+`<br><button class="btn-success btn-marker"  onclick=Cookies.remove("${element}");location.reload()>Remove</button>`).addTo(map);
+          let marker = new L.Marker(markerLocation, markerOptions).bindPopup(`<h1>${dictioPlaces[element][2]}</h1><br><p>${generatedPoet}</p>`).addTo(map);
       
-          // var marker = new L.Marker(markerLocation, markerOptions)
-             
-             
-              map.addLayer(marker)
-  
-            });
+          map.addLayer(marker)
+    }); 
 
-        cookiesDone.forEach(element => {
-            var markerOptions = {
-                title: element,
-                clickable: true,
-                icon: doneIcon
-                }
-                console.log(element)
-            
-                let lon = dictioPlaces[element][0][0];
-                let lat = dictioPlaces[element][0][1];
-        
-                let markerLocation = new L.LatLng(lon,lat);
-                let marker = new L.Marker(markerLocation, markerOptions).bindPopup(`<h1>${dictioPlaces[element][2]}</h1><br>`).addTo(map);
-            
-                map.addLayer(marker)
-        
-                    });
-  
-  
-  
-      var polypoints = []
-  
-  poly()
+  var polypoints = []
   
   function poly(){
       cookies.forEach(element => {
@@ -199,9 +188,10 @@ var dictioPlaces = {
         });
   }
   
+  poly()
   
   
-  
+  // QR STUFF
   const html5QrCode = new Html5Qrcode(/* element id */ "reader");
   
   // File based scanning
@@ -218,18 +208,16 @@ var dictioPlaces = {
     .then(qrCodeMessage => {
       // success, use qrCodeMessage
       let target= qrCodeMessage.replace('http://github.io/pnofrc/THC/','').replace('/index.html/','')
-      console.log(target)
-      // console.log(cookies.includes(target),1)
+      console.log(cookies.includes(target))
   
       if (cookies.includes(target)){
-        Cookies.set(target,1);
+        Cookies.set(target,generatedPoet);
         const index = cookies.indexOf(target);
           if (index > -1) {
               cookies.splice(index, 1);
   }
-          console.log(cookies)
-          cookiesDone.push(target);
-          localStorage.setItem(target,"donene")
+          //cookiesDone.push(target);
+          //localStorage.setItem(target,"donene")
         alert(':)')
         location.reload()
       } 
@@ -267,8 +255,26 @@ var dictioPlaces = {
   
   let lenPoints = Object.keys(polypoints).length
   
+
   arr=[]
-  
+  arr2=['M',dictioPlaces[cookiesDone][0],]
+
+  var polypointsDone = []
+
+  cookiesDone.forEach(element => {
+    let lon = dictioPlaces[element][0][0];
+    let lat = dictioPlaces[element][0][1];
+    polypoints.push([lon,lat])    
+    });
+
+    let lenPointsDone = Object.keys(polypointsDone).length
+
+
+for (let ppp = 0; ppp < cookiesDone.length; ppp++){
+  arr2.push(dictioPlaces[cookiesDone][ppp])
+}
+  console.log(arr2)
+
   function geoFindMe() {
     function success(position) {
       const latitude  = position.coords.latitude;
@@ -279,20 +285,13 @@ var dictioPlaces = {
       bezier()
     }
   
-  console.log(arr)
-
     if(!navigator.geolocation) {
       console.log('Geolocation is not supported by your browser')
     } else {
-        console.log('Locating…')
-        
+      console.log('Locating…')
       navigator.geolocation.getCurrentPosition(success);
     }
-  
   }
-  
-//   document.querySelector('#find-me').addEventListener('click', geoFindMe);
-
 
   // GPS ON
   var gps = (L.control.locate({
@@ -309,113 +308,122 @@ var dictioPlaces = {
 
 gps.start();
 
-console.log(arr.includes(['M']))
-
-      function bezier(){
-          if (arr.includes('M')){
-            arr.push('T')
-            arr.push(polypoints[0])
-          } else {
-            arr.push('M')
-            arr.push(polypoints[0])
-          }
-      polypoints.forEach(element => {
-          arr.push('T')
-          arr.push(element)
-      });
-    //   arr.push('Z')
+function bezier(){
+    if (arr.includes('M')){
+      arr.push('T')
+      arr.push(polypoints[0])
+    } else {
+      arr.push('M')
+      arr.push(polypoints[0])
+    }
+    polypoints.forEach(element => {
+      arr.push('T')
+      arr.push(element)
+    });
   }
   
-//   bezier()
-  console.log(arr)
+  function bezierDone(){
+    if (arr.includes('M')){
+      arr.push('T')
+      arr.push(polypointsDone[0])
+    } else {
+      arr.push('M')
+      arr.push(polypointsDone[0])
+    }
+    polypointsDone.forEach(element => {
+      arr.push('T')
+      arr.push(element)
+    });
+  }
 
-  
-  if (lenPoints > 0){
-  
-  
+bezier()
+// bezierDone()
+console.log(arr)
+console.log(arr2)
+
+  console.log(lenPointsDone)
+if (lenPoints > 0){
   let bez = L.curve(arr ,{color: colori[r] }).addTo(map);
-  
-  
   if (bez){
-  
     let latCentro = JSON.stringify( bez.getCenter()["lat"])
-      let lngCentro = JSON.stringify( bez.getCenter()["lng"])
-      let numeriCentro = [parseFloat(latCentro),parseFloat(lngCentro)]
-      map.panTo(L.latLng(numeriCentro[0],numeriCentro[1]));
-  } else {
-      map.panTo(L.latLng(start))
-  }
+    let lngCentro = JSON.stringify( bez.getCenter()["lng"])
+    let numeriCentro = [parseFloat(latCentro),parseFloat(lngCentro)]
+    map.panTo(L.latLng(numeriCentro[0],numeriCentro[1]));
+} else {
+    map.panTo(L.latLng(start))
+}}
+
+if (lenPointsDone > 0){
+  let bez = L.curve(arr ,{color: colori[r2] }).addTo(map);
+  if (bez){
+    let latCentro = JSON.stringify( bez.getCenter()["lat"])
+    let lngCentro = JSON.stringify( bez.getCenter()["lng"])
+    let numeriCentro = [parseFloat(latCentro),parseFloat(lngCentro)]
+    // map.panTo(L.latLng(numeriCentro[0],numeriCentro[1]));
+} else {
+    // map.panTo(L.latLng(start))
+}}
   
-  }
-  
-  
-  
-  
-  
-  
-  
-  
-   function showMap(){
-          let isEqual = JSON.stringify(cookies) === JSON.stringify(cookiesChanged);
-          console.log(isEqual);
-  
-          if ($("#map").is(':visible')) {
-            $('#map').slideUp();
-          } else {
-            if(!isEqual){
-              location.reload()
-            } else {
-              //   $('#map').css("display","block")
-              $('#map').slideDown();
-            }
-          }
-        }
-  
-  
-        var $grid = $('.grid').isotope({
-          itemSelector: '.grid-item',
-          layoutMode: 'fitRows',
-          getSortData: {
-            name: function (element) {
-              return $(element).text();
-            }
-          }
-        });
-  
-  
-  
-        $('.filter button').on("click", function () {
-          var value = $(this).attr('data-name');
-            $grid.isotope({
-              filter: value
-            });
-            localStorage.setItem("tagClass",value)
-          $('.filter button').removeClass('active');
-          $(this).addClass('active');
-        })
-        $('.sort button').on("click", function () {
-          var value = $(this).attr('data-name');
-          $grid.isotope({
-            sortBy: value
-          });
-          $('.sort button').removeClass('active');
-          $(this).addClass('active');
-        })
-  
-  
-      
-        intro =  localStorage.getItem("tagClass"); 
-  
-      if (localStorage['tagClass']){
-          console.log(intro)
-          $grid.isotope({
-                      filter:  intro,
-                  });
+
+function showMap(){
+      let isEqual = JSON.stringify(cookies) === JSON.stringify(cookiesChanged);
+      console.log(isEqual);
+
+      if ($("#map").is(':visible')) {
+        $('#map').slideUp();
       } else {
-          $grid.isotope({
-                      filter: ".All"
-                  });
+        if(!isEqual){
+          location.reload()
+        } else {
+          //   $('#map').css("display","block")
+          $('#map').slideDown();
+        }
       }
+    }
+    var $grid = $('.grid').isotope({
+      itemSelector: '.grid-item',
+      layoutMode: 'fitRows',
+      getSortData: {
+        name: function (element) {
+          return $(element).text();
+        }
+      }
+    });
+  
+  
+  
+  $('.filter button').on("click", function () {
+    var value = $(this).attr('data-name');
+      $grid.isotope({
+        filter: value
+      });
+      localStorage.setItem("tagClass",value)
+    $('.filter button').removeClass('active');
+    $(this).addClass('active');
+  })
+  $('.sort button').on("click", function () {
+    var value = $(this).attr('data-name');
+    $grid.isotope({
+      sortBy: value
+    });
+    $('.sort button').removeClass('active');
+    $(this).addClass('active');
+  })
+
+
+
+  intro =  localStorage.getItem("tagClass"); 
+
+if (localStorage['tagClass']){
+    console.log(intro)
+    $grid.isotope({
+                filter:  intro,
+            });
+} else {
+    $grid.isotope({
+                filter: ".All"
+            });
+}
   
       
   
